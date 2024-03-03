024a71508c7a0d96d07867520ec3256b4c94051b
Ibrahima FAYE <ibouddha1.if@gmail.com>
2024-03-03 08:31:02 +0000

    exo3 python for monday

---
+++ python td/Partie 3/Exo3.py
@@ -0,0 +1,42 @@
+#une phrase commence par une lettre majuscule et se termine par un point et ne contient pas de caractere special
+
+def del_space(phrase:str):
+    # Params: phrase - string with spaces at the beginning and/or end.
+    # Returns: string without leading or trailing space characters.
+        
+    invalid_word = ["&", "$","£","%","#","{","]","[","}","@", "(", ")","<", ">","/"]
+    valid_word = [",",";",".","!",":","?","'"]
+    modified_sentence =""
+
+    for i in range(0,len(phrase)):
+        if phrase[i]!=" ":
+            modified_sentence += phrase[i]
+        else:
+            if(i==len(phrase)-1 or phrase[i+1]!=" " and (phrase[i+1] not in valid_word or phrase[i-1]!=" ")) :
+                modified_sentence +=' '
+    # print("Modified Sentence is: ",modified_sentence,end= ".")
+    return modified_sentence
+
+def estValide(phrase:str)->bool:
+    valid = True
+    if(len(phrase)<25): return False
+    for i in range(0, len(phrase)):
+        if (phrase[0].islower() or phrase[-1]!="."):
+            valid = False
+        else: 
+            valid = True
+
+    return valid
+
+
+nombre_phrase = input("donnez le nombre de phrase: ")
+paragraph = ""
+for i in range(int(nombre_phrase)) :
+    phrase=str(input("Donnez la phrase "+ str(i+1)+":"))
+    
+    while not estValide(phrase):
+        print("La phrase doit commencer par une lettre majuscule et se terminer par un point et comporte au moins 25 caractères.")
+        phrase=input("Reessayez la phrase "+ str(i+1)+":")
+    
+    paragraph += del_space(del_space(phrase)) + "\n"
+print(f"{paragraph}")
