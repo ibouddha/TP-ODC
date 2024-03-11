5ae848ddf8dfba3fbf106647a2792735819fb469
Ibrahima FAYE <ibouddha1.if@gmail.com>
2024-03-11 11:12:46 +0000

    add file

---
+++ projet python/projet.py
@@ -0,0 +1,15 @@
+#ouverture du fichier csv
+with open("./"
+#declaration des structures de données:
+#parcourir les fichiers
+#verifier les lignes valides et invalides
+#récuperation de l'option de l'utilisateur
+#faire le traitement en fonction du choix de l'utilisateur
+    #afficher toutes les informations
+    #afficher une informations par son numero
+    #afficher les cinq premieres
+    #ajouter une informations
+    #modifier une informations invalides
+    #quitter le programme
+#repeter les etapes precedentes jusqu'a ce que l'utilisateur decide de quitter
+#afficher les informations par pagination
